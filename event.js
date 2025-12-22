72f9b7ae9adb325310a3094cdf1e3769d3c14bba
R-Akila-2006 <akilaravikumar05@gmail.com>
2025-12-23 01:27:10 +0530

    modified

--- event.js
+++ event.js
@@ -1,1 +1,9 @@
+//“Whenever I make changes and click the button, I want it to change from ‘Hello World ’ to ‘Bye World’ every time.”//
 console.log(document.getElementById("one"))
+var a = document.getElementById("one")
+
+function change(){
+    a.textContent = "Bye World"
+
+}
+
