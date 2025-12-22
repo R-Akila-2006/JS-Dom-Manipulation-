b78a5760d5df48062a989a5490668e8f0704ab25
R-Akila-2006 <akilaravikumar05@gmail.com>
2025-12-23 02:17:37 +0530

    Added two input fields and display sum on button click

--- event.js
+++ event.js
@@ -6,4 +6,32 @@ function change(){
     a.textContent = "Bye World"
 
 }
+// onmouseover//
+console.log(document.getElementById("two"))
+var b= document.getElementById("two")
+function mouse(){
+    b.textContent = "POTTER"
+}
+// 🎯 challenge -1//
+//1.create 2 input box with id 'n1' and 'n2'//
+//2. on a button click take the two values from both the boxes and store it in a varible called "total"//
+//3 .create a p tag with id 'result' . Everytime user click add button the added result should be displayed in p tag//
+var b1 = document.getElementById("n1")
+var b2= document.getElementById("n2")
+var sum = document.getElementById("total")
+var re = document.getElementById("result")
 
+//In this, don’t use textContent. Use value instead.”//
+var valu1 = Number(b1.value)
+var valu2 = Number(b2.value)
+var to = valu1 + valu2
+console.log(to)
+// now it print '35' but I want 8//
+// using Number() function it used to convert string to number//
+function add(){
+    var valu1 = Number(b1.value)
+    var valu2 = Number(b2.value)
+    var to = valu1 + valu2
+    console.log(to)
+    re.textContent = to
+}
